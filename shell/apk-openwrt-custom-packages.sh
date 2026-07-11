03152836baf700354f74f8b08ffabda4d478faed
Arthur97172 <arthur97172@outlook.com>
2026-07-11 09:10:25 +0800

    Update apk-openwrt-custom-packages.sh

--- shell/apk-openwrt-custom-packages.sh
+++ shell/apk-openwrt-custom-packages.sh
@@ -3,7 +3,10 @@
 # 启用第三方插件时取消对应注释
 
 # argon主题 (argon)
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-argon luci-theme-material"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-argon"
+
+# material (material)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-material"
 
 # aurora主题 (aurora)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-aurora-config luci-theme-aurora luci-i18n-aurora-config-zh-cn"
