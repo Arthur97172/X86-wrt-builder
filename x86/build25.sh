3a60daf2159e447a3739e7194bad360ee8b94a25
Arthur97172 <arthur97172@outlook.com>
2026-07-11 09:06:24 +0800

    Update build25.sh

--- x86/build25.sh
+++ x86/build25.sh
@@ -42,7 +42,7 @@ PACKAGES="$PACKAGES kmod-usb-ohci kmod-usb-ohci-pci kmod-usb2-pci usbutils kmod-
 PACKAGES="$PACKAGES kmod-fs-f2fs kmod-fs-vfat kmod-nf-nathelper kmod-nf-nathelper-extra kmod-nft-offload kmod-nft-tproxy"
 
 # [LuCI 界面和主题]
-PACKAGES="$PACKAGES luci-base luci-i18n-base-zh-cn luci-mod-admin-full luci-theme-material"
+PACKAGES="$PACKAGES luci-base luci-i18n-base-zh-cn luci-mod-admin-full"
 
 # [常用插件]
 PACKAGES="$PACKAGES luci-app-samba4 luci-i18n-samba4-zh-cn luci-app-upnp luci-i18n-upnp-zh-cn luci-app-wol luci-i18n-wol-zh-cn luci-app-ddns luci-i18n-ddns-zh-cn luci-app-ttyd luci-i18n-ttyd-zh-cn luci-app-package-manager luci-i18n-package-manager-zh-cn"
