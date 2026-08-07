23d86abe59e049b338bdeb75e620e71640ca1628
Arthur97172 <arthur97172@outlook.com>
2026-08-07 13:53:33 +0800

    Update apk-immortalwrt-custom-packages.sh

--- shell/apk-immortalwrt-custom-packages.sh
+++ shell/apk-immortalwrt-custom-packages.sh
@@ -8,8 +8,11 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES ddns-scripts-cloudflare luci-app-ddns luci-i18
 #samb文件共享
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-samba4 luci-i18n-samba4-zh-cn"
 
-#定时关机
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-autoreboot luci-i18n-autoreboot-zh-cn"
+#timecontrol上网时间控制
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-timecontrol luci-i18n-timecontrol-zh-cn"
+
+#watchdog看门狗
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES watchdog luci-app-watchdog luci-i18n-watchdog-zh-cn"
 
 # adguardhome广告拦截 (adguardhome)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES adguardhome luci-app-adguardhome luci-i18n-adguardhome-zh-cn"
