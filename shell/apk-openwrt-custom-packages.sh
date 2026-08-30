267be1311731aa2236bbb46418e4bfd734d90d36
Arthur97172 <arthur97172@outlook.com>
2026-08-30 15:02:29 +0800

    Update apk-openwrt-custom-packages.sh

--- shell/apk-openwrt-custom-packages.sh
+++ shell/apk-openwrt-custom-packages.sh
@@ -60,7 +60,7 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-partexp luci-i18n-partexp-zh-cn"
 
 # passwall依赖 (passwall)
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES geoview xray-core sing-box hysteria"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES geoview"
 
 # passwall代理面板 (passwall)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-passwall luci-i18n-passwall-zh-cn"
