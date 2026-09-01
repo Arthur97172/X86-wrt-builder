2aabce88ff32a8b760a39faf8c22bbbdfe48f1fc
Arthur97172 <arthur97172@outlook.com>
2026-09-01 10:27:39 +0800

    Update apk-immortalwrt-custom-packages.sh

--- shell/apk-immortalwrt-custom-packages.sh
+++ shell/apk-immortalwrt-custom-packages.sh
@@ -57,7 +57,8 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-partexp luci-i18n-partexp-zh-cn"
 
 # passwall代理面板 (passwall)
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES geoview shadowsocksr-libev-ssr-local shadowsocksr-libev-ssr-redir luci-app-passwall luci-i18n-passwall-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-passwall luci-i18n-passwall-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES chinadns-ng geoview shadowsocksr-libev-ssr-local shadowsocksr-libev-ssr-redir"
 
 # poweroffdevice关机 (poweroffdevice)
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-poweroffdevice luci-i18n-poweroffdevice-zh-cn"
