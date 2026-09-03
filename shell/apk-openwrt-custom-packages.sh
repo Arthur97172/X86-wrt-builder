3b48fe0749fba4414289c7b9b38a874e820723f4
Arthur97172 <arthur97172@outlook.com>
2026-09-03 11:01:45 +0800

    Update apk-openwrt-custom-packages.sh

--- shell/apk-openwrt-custom-packages.sh
+++ shell/apk-openwrt-custom-packages.sh
@@ -64,7 +64,7 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES geoview"
 
 # passwall代理面板 (passwall)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-passwall luci-i18n-passwall-zh-cn"
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES chinadns-ng geoview shadowsocksr-libev-ssr-local shadowsocksr-libev-ssr-redir"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES sing-box chinadns-ng geoview shadowsocksr-libev-ssr-local shadowsocksr-libev-ssr-redir"
 
 # poweroffdevice关机 (poweroffdevice)
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-poweroffdevice luci-i18n-poweroffdevice-zh-cn"
